2a878f1752c6eb7389c2ee3283441605a1f6216d
Nathaniel Hutchins <nhutch@amazon.com>
2019-08-10 15:16:48 -0700

    remove unit tests and cover a few more edge cases with integ tests
    I've decided that "intent-based" testing is better than "effect-based". As a result, all tests are now integration tests -- they start with the initialization of a store, perform some action on it, and then check the result. The tests are slightly more resillient this way and are easier to read.
    
    In addition, we won't need to expose internal behaviour nearly as much. (e.g. select.set returning an action instead of, say, the value we set it to).

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,4 +1,3 @@
-// import { initializeStore } from '../dist/no-boilerplate-redux'
 import { initializeStore } from '../src/initialize-store'
 
 // test as much of the functionality that users will experience as reasonable
@@ -63,7 +62,7 @@ describe('store with preloadedState and functions', () => {
   test("store initializes", () => {
     store = initializeStore({
       preloadedState: {
-        songsByArtist: {
+        songPlaysByArtist: {
           'Talking Heads': {
             'Burning Down The House': 11,
             'City of Dreams': 14,
@@ -77,7 +76,7 @@ describe('store with preloadedState and functions', () => {
     })
 
     expect(store.getState()).toEqual({
-      songsByArtist: {
+      songPlaysByArtist: {
         'Talking Heads': {
           'Burning Down The House': 11,
           'City of Dreams': 14,
@@ -88,14 +87,14 @@ describe('store with preloadedState and functions', () => {
     })
   })
 
-  test('can increment already extant value', () => {
-    store.select('songsByArtist').set(songsByArtist => {
-      songsByArtist['Talking Heads']['Burning Down The House']++
-      return songsByArtist
+  test('can update a store part using a function w/o a path', () => {
+    store.select('songPlaysByArtist').set(songPlaysByArtist => {
+      songPlaysByArtist['Talking Heads']['Burning Down The House']++
+      return songPlaysByArtist
     })
 
     expect(store.getState()).toEqual({
-      songsByArtist: {
+      songPlaysByArtist: {
         'Talking Heads': {
           'Burning Down The House': 12,
           'City of Dreams': 14,
@@ -106,12 +105,12 @@ describe('store with preloadedState and functions', () => {
     })
   })
 
-  test('add completely new value via path', () => {
-    store.select('songsByArtist', '["Talking Heads"]["Psycho Killer"]')
-      .set(psychoKillerPlays => (psychoKillerPlays || 0) + 1)
+  test('can update a store part using a function w/ a path', () => {
+    store.select('songPlaysByArtist', '["Talking Heads"]["Psycho Killer"]')
+      .set((psychoKillerPlays = 0) => (psychoKillerPlays) + 1)
 
     expect(store.getState()).toEqual({
-      songsByArtist: {
+      songPlaysByArtist: {
         'Talking Heads': {
           'Burning Down The House': 12,
           'City of Dreams': 14,
@@ -123,27 +122,7 @@ describe('store with preloadedState and functions', () => {
     })
   })
 
-  test('can add completely new value via function', () => {
-    store.select('songsByArtist', '["Talking Heads"]')
-      .set(talkingHeadsSongs => {
-        talkingHeadsSongs["And She Was"] = 1
-        return talkingHeadsSongs
-      })
-    expect(store.getState()).toEqual({
-      songsByArtist: {
-        'Talking Heads': {
-          'Burning Down The House': 12,
-          'City of Dreams': 14,
-          'Stay up Late': 8,
-          'Psycho Killer': 1,
-          'And She Was': 1
-        }
-      },
-      artists: {}
-    })
-  })
-
-  test('can increment already extant value', () => {
+  test('can replace a path on a store part with a value', () => {
     store.select('artists', '["Talking Heads"].members').set([
       'David Byrne',
       'Chris Frantz',
@@ -152,14 +131,35 @@ describe('store with preloadedState and functions', () => {
     ])
 
     expect(store.getState()).toEqual({
-      songsByArtist: {
+      songPlaysByArtist: {
         'Talking Heads': {
           'Burning Down The House': 12,
           'City of Dreams': 14,
           'Stay up Late': 8,
-          'Psycho Killer': 1,
-          'And She Was': 1
+          'Psycho Killer': 1
         }
+      },
+      artists: {
+        'Talking Heads': {
+          'members': [
+            'David Byrne',
+            'Chris Frantz',
+            'Tina Weymouth',
+            'Jerry Harrison'
+          ]
+        }
+      }
+    })
+  })
+
+  test('can replace an entire store part with a value', () => {
+    store.select('songPlaysByArtist').set({
+      'Talking Heads': {}
+    })
+
+    expect(store.getState()).toEqual({
+      songPlaysByArtist: {
+        'Talking Heads': {}
       },
       artists: {
         'Talking Heads': {
@@ -175,8 +175,8 @@ describe('store with preloadedState and functions', () => {
   })
 })
 
-describe('handling undefined (or null) values', () => {
-  test('the entire store can handle it', () => {
+describe('user can set parts of the store to undefined (or null) values', () => {
+  test('user can set a store part to undefined and back with a value', () => {
     let store = initializeStore()
 
     // set a store from nothing
@@ -217,9 +217,9 @@ describe('handling undefined (or null) values', () => {
     })
   })
 
-  test('the entire store can handle it (using fns)', () => {
+  test('user can set a store part to undefined and back with a function', () => {
     let store = initializeStore()
-    
+
     // set the store to something
     store.select('todos').set(() => ([
       { id: 0, value: "finish writing unit tests" },
@@ -257,7 +257,7 @@ describe('handling undefined (or null) values', () => {
     })
   })
 
-  test('a part of the store can handle it', () => {
+  test('user can set a path on a store part to undefined and back with a value', () => {
     let store = initializeStore()
 
     // store goes from nothing to something
@@ -285,9 +285,9 @@ describe('handling undefined (or null) values', () => {
     })
   })
 
-  test('a part of the store can handle it (using fns)', () => {
+  test('user can set a path on a store part to undefined and back with a function', () => {
     let store = initializeStore()
-    
+
     // can the store handle being deeply set from nothing?
     store.select('one', 'step.too').set(() => ({ few: true }))
 
@@ -314,7 +314,96 @@ describe('handling undefined (or null) values', () => {
   })
 })
 
-describe("store can be modified using `set` pattern", () => {
+describe('user can customize the action being fired', () => {
+  let store = initializeStore()
+  let dispatch = jest.spyOn(store, 'dispatch')
+
+  test('user can customize the action with a string', () => {
+    store.select('todos').set((todos) => {
+      if (todos === null)
+        todos = []
+
+      todos.push({
+        id: todos.length,
+        value: "make sure string action customization works"
+      })
+
+      return todos
+    }, 'with_push')
+
+    let lastCallArguments = dispatch.mock.calls[dispatch.mock.calls.length - 1]
+    let firstArgument = lastCallArguments[0]
+
+    expect(firstArgument).toEqual({
+      type: 'SET_TODOS_WITH_PUSH',
+      payload:
+      {
+        path: undefined,
+        value: undefined,
+        fn: expect.any(String)
+      },
+      meta: { nbpr: 'todos' }
+    })
+
+    expect(store.getState()).toEqual({
+      redux_loaded: true,
+      todos: [{
+        id: 0,
+        value: "make sure string action customization works"
+      }]
+    })
+  })
+
+  test('user can customize the action with an object', () => {
+
+    store.select('todos').set((todos) => {
+      if (todos === null)
+        todos = []
+
+      todos.push({
+        id: todos.length,
+        value: "make sure object action customization works"
+      })
+
+      return todos
+    }, {
+      type: "PUSH_TODO",
+      payload: {
+        someExtraData: 10
+      },
+      meta: {
+        moreExtraData: 11
+      }
+    })
+
+    let lastCallArguments = dispatch.mock.calls[dispatch.mock.calls.length - 1]
+    let firstArgument = lastCallArguments[0]
+
+    expect(firstArgument).toEqual({
+      type: 'PUSH_TODO',
+      payload: {
+        someExtraData: 10,
+        path: undefined,
+        value: undefined,
+        fn: expect.any(String)
+      },
+      meta: { 
+        moreExtraData: 11,
+        nbpr: 'todos' 
+      }
+    })
+
+    expect(store.getState()).toEqual({
+      redux_loaded: true,
+      todos: [
+        { id: 0, value: "make sure string action customization works" },
+        { id: 1, value: "make sure object action customization works" }
+    ]
+    })
+  })
+})
+
+describe("user can modify the store using `.set()`", () => {
   test("using a fn and modifying the `store` object performs a merge", () => {
     let store = initializeStore({
       preloadedState: {
@@ -400,42 +489,62 @@ describe("store can be modified using `set` pattern", () => {
       isAuthenticated: true
     })
   })
+
+  test("throws when user tries to return a non-object from a .set()", () => {
+    let store = initializeStore()
+
+    expect(() => {
+      store.set(() => {
+        return 'MynockSpit'
+      })
+    }).toThrow()
+  })
+
+  test("throws when user tries to .set() to a value that isn't an object or function", () => {
+    let store = initializeStore()
+
+    expect(() => {
+      store.set('MynockSpit')
+    }).toThrow()
+  })
 })
 
-describe("store can be read using `get` pattern", () => {
-  test("using store.select(...).get()", () => {
-    let store = initializeStore({
-      preloadedState: {
-        todos: [
-          { id: 0, value: "finish writing unit tests" },
-          { id: 1, value: "write integ tests" }
-        ]
-      }
-    })
+test("user can read the store using `.select().get()`", () => {
+  let preloadedState = {
+    todos: [
+      { id: 0, value: "finish writing unit tests" },
+      { id: 1, value: "write integ tests" }
+    ]
+  }
+  let store = initializeStore({ preloadedState })
 
-    // update using a function
-    const firstTodoText = store.select('todos', '0.value').get(false)
-    const tenthTodoText = store.select('todos', '10.value').get(false)
+  // update using a function
+  const getWithoutPath = store.select('todos').get()
+  expect(getWithoutPath).toEqual(preloadedState.todos)
 
-    expect(firstTodoText).toEqual("finish writing unit tests")
-    expect(tenthTodoText).toEqual(false)
-  })
-  
-  test("using store.get()", () => {
-    let store = initializeStore({
-      preloadedState: {
-        todos: [
-          { id: 0, value: "finish writing unit tests" },
-          { id: 1, value: "write integ tests" }
-        ]
-      }
-    })
+  const firstTodoText = store.select('todos', '0.value').get(false)
+  expect(firstTodoText).toEqual("finish writing unit tests")
 
-    // update using a function
-    const firstTodoText = store.get('todos.0.value', false)
-    const tenthTodoText = store.get('todos.10.value', false)
+  const tenthTodoText = store.select('todos', '10.value').get(false)
+  expect(tenthTodoText).toEqual(false)
 
-    expect(firstTodoText).toEqual("finish writing unit tests")
-    expect(tenthTodoText).toEqual(false)
-  })
+  const noStoreValue = store.select('nonExistant').get()
+  expect(noStoreValue).toEqual(undefined)
+})
+
+test("user can read the store using `.get()`", () => {
+  let preloadedState = {
+    todos: [
+      { id: 0, value: "finish writing unit tests" },
+      { id: 1, value: "write integ tests" }
+    ]
+  }
+  let store = initializeStore({ preloadedState })
+
+  // update using a function
+  const getWithoutPath = store.get()
+  expect(getWithoutPath).toEqual(preloadedState)
+
+  // any argument causes store.get to throw
+  expect(() => { store.get('path.to.whatever') }).toThrow()
 })
